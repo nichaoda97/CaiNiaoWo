7e4d4ec5503d78348bf48b986049b8b5903a4959
ncd <nichaoda@foxmail.com>
2021-06-06 10:16:20 +0800

    BaseActivity和BaseFragment进一步封装,采取了不同的方式实现将DataBinding包括进去的封装

--- common/src/main/java/com/cainiaowo/common/base/BaseActivity.kt
+++ common/src/main/java/com/cainiaowo/common/base/BaseActivity.kt
@@ -1,18 +1,71 @@
 package com.cainiaowo.common.base
 
+import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LiveData
+import com.blankj.utilcode.util.LogUtils
+import com.cainiaowo.common.ktx.bindView
 import com.cainiaowo.common.ktx.viewLifecycleOwner
 
 /**
  * Activity抽象基类
  */
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<ActivityDataBinding : ViewDataBinding> : AppCompatActivity {
+
+    constructor() : super()
+
+    constructor(@LayoutRes layoutId: Int) : super(layoutId)
+
+    protected lateinit var mBinding: ActivityDataBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = bindView<ActivityDataBinding>(getLayoutRes()).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+        initView()
+        initConfig()
+        initData()
+    }
+
+    @LayoutRes
+    abstract fun getLayoutRes(): Int
 
     /**
-     * 扩展LiveData的observe函数
+     * 初始化view
      */
-    protected fun <T : Any> LiveData<T>.observe(block: (T) -> Unit) {
+    open fun initView() {
+        LogUtils.d("${this.javaClass.simpleName} 初始化 initView")
+    }
+
+    /**
+     * 初始化配置
+     */
+    open fun initConfig() {
+        LogUtils.d("${this.javaClass.simpleName} 初始化 initConfig")
+    }
+
+    /**
+     * 初始化数据
+     */
+    open fun initData() {
+        LogUtils.d("${this.javaClass.simpleName} 初始化 initData")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (this::mBinding.isInitialized) {
+            mBinding.unbind()
+        }
+    }
+
+    /**
+     * 扩展用于LiveData便捷写法的observe函数
+     * [block] 响应change变化的逻辑块
+     */
+    protected fun <T : Any> LiveData<T>.observe(block: (T?) -> Unit) {
         this.observe(viewLifecycleOwner) { data ->
             block.invoke(data)
         }
--- common/src/main/java/com/cainiaowo/common/base/BaseFragment.kt
+++ common/src/main/java/com/cainiaowo/common/base/BaseFragment.kt
@@ -1,8 +1,14 @@
 package com.cainiaowo.common.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
+import com.blankj.utilcode.util.LogUtils
 
 /**
  * Fragment抽象基类
@@ -14,9 +20,58 @@ abstract class BaseFragment : Fragment {
     constructor(@LayoutRes layoutId: Int) : super(layoutId)
 
     /**
-     * 扩展LiveData的observe函数
+     * UI的ViewDataBinding对象
      */
-    protected fun <T : Any> LiveData<T>.observe(block: (T) -> Unit) {
+    private var mBinding: ViewDataBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(getLayoutRes(), container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mBinding = bindView(view, savedInstanceState)
+        mBinding?.lifecycleOwner = viewLifecycleOwner
+        initConfig()
+        initData()
+    }
+
+    @LayoutRes
+    abstract fun getLayoutRes(): Int
+
+    /**
+     * view和ViewDataBinding绑定
+     */
+    abstract fun bindView(view: View, savedInstanceState: Bundle?): ViewDataBinding
+
+    /**
+     * 初始化配置
+     */
+    open fun initConfig() {
+        LogUtils.d("${this.javaClass.simpleName} 初始化 initConfig")
+    }
+
+    /**
+     * 初始化数据
+     */
+    open fun initData() {
+        LogUtils.d("${this.javaClass.simpleName} 初始化 initData")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding?.unbind()
+    }
+
+    /**
+     * 扩展用于LiveData便捷写法的observe函数
+     * [block] 响应change变化的逻辑块
+     */
+    protected fun <T : Any> LiveData<T>.observe(block: (T?) -> Unit) {
         this.observe(viewLifecycleOwner) { data ->
             block.invoke(data)
         }
