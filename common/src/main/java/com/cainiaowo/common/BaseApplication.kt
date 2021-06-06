6153d30acc16b247374fc1bf921e43828bd22e47
ncd <nichaoda@foxmail.com>
2021-06-06 22:56:24 +0800

    添加dokit依赖,Application扩展,BaseApplication进一步封装,Application初步实现

--- common/src/main/java/com/cainiaowo/common/BaseApplication.kt
+++ common/src/main/java/com/cainiaowo/common/BaseApplication.kt
@@ -1,6 +1,7 @@
 package com.cainiaowo.common
 
 import android.app.Application
+import com.blankj.utilcode.util.LogUtils
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -15,12 +16,26 @@ abstract class BaseApplication : Application() {
         super.onCreate()
 
         startKoin {
-            androidLogger(Level.ERROR)  // log level Error方能保证这句话不会报错,要么就不写这个
+            androidLogger(Level.ERROR)
 
             // 把application传给Koin依赖框架,可以在其它地方使用
             androidContext(this@BaseApplication)
 
-            //modules()
+            // modules()    // 依赖注入模块
         }
+        initConfig()
+        initData()
+
+        LogUtils.d("BaseApplication onCreate")
     }
+
+    /**
+     * 初始化配置
+     */
+    protected open fun initConfig() {}
+
+    /**
+     * 初始化数据
+     */
+    protected open fun initData() {}
 }
---
+++ common/src/main/java/com/cainiaowo/common/ktx/ApplicationKtx.kt
@@ -0,0 +1,10 @@
+package com.cainiaowo.common.ktx
+
+import android.app.Application
+
+/**
+ * Application扩展
+ */
+
+val Application.application: Application
+    get() = this
