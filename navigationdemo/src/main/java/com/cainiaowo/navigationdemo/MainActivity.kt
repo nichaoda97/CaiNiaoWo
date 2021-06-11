8e3dd580b5ad7807569cc070aeeaa0f949ccd1fa
ncd <nichaoda@foxmail.com>
2021-06-11 22:43:47 +0800

    navigation Toolbar返回Demo

--- navigationdemo/src/main/java/com/cainiaowo/navigationdemo/MainActivity.kt
+++ navigationdemo/src/main/java/com/cainiaowo/navigationdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cainiaowo.navigationdemo
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,14 +11,16 @@ import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
 
-        // region ActionBar中的切换
+        // region Toolbar中的切换
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -29,5 +32,9 @@ class MainActivity : AppCompatActivity() {
         // endregion
 
         navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
