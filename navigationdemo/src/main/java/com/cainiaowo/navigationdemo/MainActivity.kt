a84a340a8b6391588d99626fc2c85ceb6bb9f09e
ncd <nichaoda@foxmail.com>
2021-06-12 10:59:43 +0800

    BottomNavigationView扩展支持navigation

--- navigationdemo/src/main/java/com/cainiaowo/navigationdemo/MainActivity.kt
+++ navigationdemo/src/main/java/com/cainiaowo/navigationdemo/MainActivity.kt
@@ -3,38 +3,47 @@ package com.cainiaowo.navigationdemo
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        // 扩展方式
+        setupBottomNavigationBar()
+    }
 
-        navController = findNavController(R.id.nav_host_fragment)
 
-        // region Toolbar中的切换
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
+    private var currentNavController: LiveData<NavController>? = null
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+
+        val navGraphIds =
+            listOf(R.navigation.graph_home, R.navigation.graph_dash, R.navigation.graph_notify)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        // endregion
 
-        navView.setupWithNavController(navController)
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this) { navController ->
+            setupActionBarWithNavController(navController)
+        }
+        currentNavController = controller
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
