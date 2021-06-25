ed421e46cb1dcb0fd62636b5d2a7f507f012f403
ncd <nichaoda@foxmail.com>
2021-06-25 23:17:23 +0800

    MineFragment+MineViewModel+DataBinding获取Room数据表中的用户信息并显示,登出时删除数据表中的用户信息

--- mine/src/main/java/com/cainiaowo/mine/MineFragment.kt
+++ mine/src/main/java/com/cainiaowo/mine/MineFragment.kt
@@ -3,19 +3,40 @@ package com.cainiaowo.mine
 import android.os.Bundle
 import android.view.View
 import androidx.databinding.ViewDataBinding
+import com.alibaba.android.arouter.launcher.ARouter
 import com.cainiaowo.common.base.BaseFragment
 import com.cainiaowo.mine.databinding.FragmentMineBinding
+import com.cainiaowo.service.repo.CaiNiaoDbHelper
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
  * 我的
  */
 class MineFragment : BaseFragment() {
+
+    private val viewModel: MineViewModel by viewModel()
+
     override fun getLayoutRes(): Int {
         return R.layout.fragment_mine
     }
 
     override fun bindView(view: View, savedInstanceState: Bundle?): ViewDataBinding {
 
-        return FragmentMineBinding.bind(view)
+        return FragmentMineBinding.bind(view).apply {
+            vm = viewModel
+
+            // UI登出操作
+            btnLogoutMine.setOnClickListener {
+                CaiNiaoDbHelper.deleteUserInfo(requireContext())
+                ARouter.getInstance().build("/login/login").navigation()
+            }
+        }
+    }
+
+    override fun initData() {
+        super.initData()
+        CaiNiaoDbHelper.getLiveUserInfo(requireContext()).observe {
+            viewModel.liveUser.value = it
+        }
     }
 }
--- mine/src/main/java/com/cainiaowo/mine/MineViewModel.kt
+++ mine/src/main/java/com/cainiaowo/mine/MineViewModel.kt
@@ -1,10 +1,14 @@
 package com.cainiaowo.mine
 
+import androidx.lifecycle.MutableLiveData
 import com.cainiaowo.common.base.BaseViewModel
+import com.cainiaowo.service.repo.CaiNiaoUserInfo
 
 /**
  * 我的界面逻辑的viewModel
  */
 class MineViewModel : BaseViewModel() {
+
+    val liveUser = MutableLiveData<CaiNiaoUserInfo>()
 
 }
